7afafc503154eb7fa7f652441aa978fc225b0368
juanjosalaz20 <juanjosalazar2000@gmail.com>
2025-03-29 13:40:57 -0500

    Clase 27/03/25

--- ejercicios/ejercicio7.py
+++ ejercicios/ejercicio7.py
@@ -5,3 +5,15 @@ Problema: Almacenar los nombres de 3 ciudades en una tupla y luego mostrar:
 
     La cantidad de caracteres de cada ciudad.
 '''
+
+C1 = input('Ingrese una ciudad: ')
+C2 = input('Ingrese una ciudad diferente a la anterior: ')
+C3 = input('Ingrese una ciudad diferente a la anterior: ')
+
+Tupla = (C1, C2, C3)
+
+print('Esta es la primera ciudad que ingresaste:',Tupla[0])
+print('Esta es la ultima ciudad que ingresaste:',Tupla[2])
+print('Esta es la cantidad de caracteres que tiene la primera ciudad:',len(Tupla[0]))
+print('Esta es la cantidad de caracteres que tiene la segunda ciudad:',len(Tupla[1]))
+print('Esta es la cantidad de caracteres que tiene la ultima ciudad:',len(Tupla[2]))
