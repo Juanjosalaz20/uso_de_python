c319d3a58f362e376b10f8db62d34d624244240b
juanjosalaz20 <juanjosalazar2000@gmail.com>
2025-03-30 12:24:07 -0500

    Clase 27/03/25

--- ejercicios/ejercicio8.py
+++ ejercicios/ejercicio8.py
@@ -4,4 +4,8 @@
 
     La intersección de ambos conjuntos'''
     
-    
+Conjunto1 = set(input('Ingrese un conjunto de numeros: '))
+Conjunto2 = set(input('Ingrese otro conjunto de numeros: '))
+
+print('Esta es la union de los dos conjuntos:', Conjunto1|Conjunto2) 
+print('Esta es la interseccion de ambos conjuntos:', Conjunto1 & Conjunto2)
