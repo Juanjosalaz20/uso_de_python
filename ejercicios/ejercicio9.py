53a658480298a75d1319a7774559aaeadb0e482c
juanjosalaz20 <juanjosalazar2000@gmail.com>
2025-03-31 15:02:34 -0500

    CLASE 27/03/25

--- ejercicios/ejercicio9.py
+++ ejercicios/ejercicio9.py
@@ -2,3 +2,16 @@
 Problema: Crear un diccionario con las calificaciones de 3 estudiantes 
 y permitir que el usuario consulte la calificación de uno de ellos.
 '''
+Calificaciones = {'Pepe': {'Matematicas': 4.1, 'Ciencia': 4.5, 'Quimica': 3.8}, 
+                  'Laura': {'Matematicas': 4.7, 'Ciencia': 4.8, 'Quimica': 4.3},  
+                  'Jose':{'Matematicas': 3.6, 'Ciencia': 4.0, 'Quimica': 4.2}}
+
+Name = input('Ingrese el nombre del estudiante: ').title()
+
+Cal_estudiante = Calificaciones.get(Name)
+
+if Cal_estudiante:
+    print(Cal_estudiante)
+else:
+    print('El estudiante no se encuentra en la lista')
+
