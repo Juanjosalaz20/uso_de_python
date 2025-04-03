2309be2f60f2c85621972fe0f111e35120ce2e74
juanjosalaz20 <juanjosalazar2000@gmail.com>
2025-04-03 12:29:26 -0500

    CLASE 1/04/25

--- ejercicios/ejercicio10.py
+++ ejercicios/ejercicio10.py
@@ -2,12 +2,12 @@
 Problema: Crear un diccionario con algunos productos 
 y permitir que el usuario agregue un nuevo producto con su precio.
 '''
-Productos = {'Martillo': 15000, 'Cierra': 20000, 'Tornillo': 500, 'Destornillador': 8000 }
+Products = {'Martillo': 15000, 'Cierra': 20000, 'Tornillo': 500, 'Destornillador': 8000 }
 
-print('Productos: ', Productos)
+print('Productos: ', Products)
 
 key = input('Ingrese el nombre del producto que desea agregar: ').title()
 val = input('Ingrese el valor del producto: ')
 
-Productos.update({key : val})
-print('Productos: ', Productos)
+Products.update({key : val})
+print('Productos: ', Products)
