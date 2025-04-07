9022e85bafc321b3c4968d1458993cff7ce6822f
juanjosalaz20 <juanjosalazar2000@gmail.com>
2025-04-07 11:59:02 -0500

    CLASE 3/04/25

--- ejercicios/ejercicios de control de flujo/ejercicio2.py
+++ ejercicios/ejercicios de control de flujo/ejercicio2.py
@@ -11,9 +11,19 @@ age = int(input('Ingrese su edad: '))
 
 #With if
 #Puedo incluir los rangos dentro del mismo if y asi evitar definir las variables
-if age in minor:
-    print('Eres menor de edad')
-elif age in adult:
-    print('Eres mayor de edad')
-else:
-    print('Eres un adulto mayor')
+#if age in minor:
+#    print('Eres menor de edad')
+#elif age in adult:
+#    print('Eres mayor de edad')
+#else:
+#    print('Eres un adulto mayor')
+
+match age:
+    case _ if age in range(0,18):
+        print('Eres menor de edad')
+    case _ if age in range(18,65):
+        print('Eres mayor de edad')
+    case _:
+        print('Eres un adulto mayor') 
+
+    
