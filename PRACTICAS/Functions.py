4b582dfb2dbc899c293a30141dc9a8b61b2f7599
juanjosalaz20 <juanjosalazar2000@gmail.com>
2025-04-10 20:00:31 -0500

    CLASE 10/04/25

--- PRACTICAS/Functions.py
+++ PRACTICAS/Functions.py
@@ -1,21 +1,20 @@
-#def Num_pares (limit):
-    #for num in range(0, limit+1, 2):
-       # yield num
+# List_1 = [1,2,3,4,5,6,6]
+# List_2 = [5,6,7,8,9,10]
 
-#for par in Num_pares (50):
-   #print(par)
+# def inter_listas(a,b): 
+#     return list(set(a) & set(b))
 
-#def Num_impares (limit):
-    #for numi in range(1, limit+1, 2):
-        #yield numi
+# print(inter_listas(List_1, List_2))
 
-#for impar in Num_impares (20):
-    #print(impar)
+clave=1234
+intentosF=0
 
-List_1 = [1,2,3,4,5,6,6]
-List_2 = [5,6,7,8,9,10]
-
-def inter_listas(a,b): 
-    return list(set(a) & set(b))
-
-print(inter_listas(List_1, List_2))
+for intentos in range(1,4):
+    pin = int(input("ingrese clave: "))
+    if pin == clave:
+        print("ingreso exitoso")
+        break
+    else:
+        print("clave erronea")
+        intentosF+=1
+        print("intento fallido",intentosF)
